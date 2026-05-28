c09534f8d51a52e269cdb7240d1267614dee260d
sipakem <sipakemsipakem@gmail.com>
2026-05-28 14:50:59 +0700

    Update app.py

--- sipakem_backend/app.py
+++ sipakem_backend/app.py
@@ -1342,4 +1342,5 @@ def dashboard_admin():
         }), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
