7668de7650912f1d207f9910e7f1fb605a3d9cb0
sipakem <sipakemsipakem@gmail.com>
2026-05-29 14:52:33 +0700

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -36,7 +36,7 @@ export default function AdminDashboard() {
 
   const getDashboard = async () => {
     try {
-      const response = await axios.get("http://sipakembackend-production.up.railway.app/dashboard-admin");
+      const response = await axios.get("https://sipakembackend-production.up.railway.app/dashboard-admin");
 
       setDashboard(response.data);
     } catch (error) {
